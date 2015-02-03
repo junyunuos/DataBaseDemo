9c8f756116a3b5bce4078a82ee4205fb30fcef62
guoqiang <guoqiang@HXMAC.local>
2015-02-03 20:43:21 +0800

    Initial Commit

---
+++ DataBaseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataBaseDemo
+//
+//  Created by guoqiang on 15-2-3.
+//  Copyright (c) 2015年 guoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
